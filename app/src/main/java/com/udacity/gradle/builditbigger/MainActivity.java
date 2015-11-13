08905d6a0932c533e5a8e95647d1ce7741aafe9a
Elias Myronidis <elias.myronidis@hotmail.com>
2015-11-13 13:43:58 +0200

    - add GCE Module which pulls jokes from Java library.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.udacity.gradle.androidjoke.AndroidJokeActivity;
-import com.udacity.gradle.javajoke.Jokes;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,17 +53,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
+        // STEP 1
         // Create an instance of Jokes from Java library
-        Jokes joke = new Jokes();
-
+        // Jokes joke = new Jokes();
         // Show a Toast when button is clicked.
         // Toast.makeText(this, joke.getJoke(), Toast.LENGTH_LONG).show();
 
+        // STEP 2
         // Send a joke created from Jokes to AndroidJokeActivity.
-        Intent androidIntent = new Intent(this, AndroidJokeActivity.class);
-        androidIntent.putExtra(AndroidJokeActivity.JOKE_KEY, joke.getJoke());
-        startActivity(androidIntent);
+        // Intent androidIntent = new Intent(this, AndroidJokeActivity.class);
+        // androidIntent.putExtra(AndroidJokeActivity.JOKE_KEY, joke.getJoke());
+        // startActivity(androidIntent);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+    }
+}
+
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - turn off compression when running against local devappserver
+
+                    // localhost's IP address in Android emulator
+                    // .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+
+                    // localhost's IP address in Genymotion
+                    .setRootUrl("http://10.0.3.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 
+    @Override
+    protected void onPostExecute(String result) {
+        // Toast.makeText(context, result, Toast.LENGTH_LONG).show();
 
+        // Retrieve joke from GCE and launch the activity from the Android Library to display it.
+        Intent androidIntent = new Intent(context, AndroidJokeActivity.class);
+        androidIntent.putExtra(AndroidJokeActivity.JOKE_KEY, result);
+        context.startActivity(androidIntent);
+    }
 }
