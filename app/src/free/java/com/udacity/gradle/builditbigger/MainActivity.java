8fe822f6e847a5f31f34ad667a381758d6f186f8
Elias Myronidis <elias.myronidis@hotmail.com>
2015-11-16 16:09:08 +0200

    - add interstitial ad to free flavor.

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,179 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.androidjoke.AndroidJokeActivity;
+import com.udacity.gradle.builditbigger.backend.jokeBeanApi.JokeBeanApi;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                new EndpointsAsyncTask(mContext).execute();
+                requestNewInterstitial();
+            }
+        });
+
+        requestNewInterstitial();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view){
+        // STEP 1
+        // Create an instance of Jokes from Java library
+        // Jokes joke = new Jokes();
+        // Show a Toast when button is clicked.
+        // Toast.makeText(this, joke.getJoke(), Toast.LENGTH_LONG).show();
+
+        // STEP 2
+        // Send a joke created from Jokes to AndroidJokeActivity.
+        // Intent androidIntent = new Intent(this, AndroidJokeActivity.class);
+        // androidIntent.putExtra(AndroidJokeActivity.JOKE_KEY, joke.getJoke());
+        // startActivity(androidIntent);
+
+        Toast.makeText(MainActivity.this, "We are in free edition", Toast.LENGTH_SHORT).show();
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            new EndpointsAsyncTask(this).execute();
+        }
+
+    }
+}
+
+class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+    private static JokeBeanApi myApiService = null;
+    private Context mContext;
+
+    private EndpointsAsyncTaskListener mListener = null;
+    private Exception mError = null;
+
+    public EndpointsAsyncTask(Context context){
+        mContext = context;
+
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if(myApiService == null) {  // Only do this once
+            JokeBeanApi.Builder builder = new JokeBeanApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - turn off compression when running against local devappserver
+
+                    // localhost's IP address in Android emulator
+                    // .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+
+                    // localhost's IP address in Genymotion
+                    .setRootUrl("http://10.0.3.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        try {
+            return myApiService.showJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    public EndpointsAsyncTask setListener(EndpointsAsyncTaskListener listener) {
+        this.mListener = listener;
+        return this;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        // Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+
+        if (this.mListener != null)
+            this.mListener.onComplete(result, mError);
+
+        // Retrieve joke from GCE and launch the activity from the Android Library to display it.
+        Intent androidIntent = new Intent(mContext, AndroidJokeActivity.class);
+        androidIntent.putExtra(AndroidJokeActivity.JOKE_KEY, result);
+        mContext.startActivity(androidIntent);
+    }
+
+    @Override
+    protected void onCancelled() {
+        if (this.mListener != null) {
+            mError = new InterruptedException("AsyncTask cancelled");
+            this.mListener.onComplete(null, mError);
+        }
+    }
+
+    public static interface EndpointsAsyncTaskListener {
+        public void onComplete(String message, Exception e);
+    }
+}
